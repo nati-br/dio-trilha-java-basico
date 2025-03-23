ced8b17f7d82db85c6c6e06034cd3a57ec23c410
Natália <48138054+nati-br@users.noreply.github.com>
2025-03-23 14:35:43 -0300

    finalizando aula operadores

--- tipos-variaveis/src/edu/natalia/operadores/Operadores.java
+++ tipos-variaveis/src/edu/natalia/operadores/Operadores.java
@@ -1,7 +1,4 @@
 package edu.natalia.operadores;
-
-import javafx.beans.property.adapter.JavaBeanBooleanProperty;
-
 public class Operadores {
     public static void main(String[] args) throws Exception {
     /* CONCATENAÇÃO
@@ -23,7 +20,7 @@ public class Operadores {
     System.out.println(concatenacao);
     */
 
-    /* NUMEROA NEGATIVO E POSITIVO
+    /*NUMEROA NEGATIVO E POSITIVO
     int numero = 5;
     System.out.println(- numero); 
     System.out.println(numero); // numero n fica negativo
@@ -33,7 +30,70 @@ public class Operadores {
     numero = numero *-1; // para torna o numero positivo novamente
     System.out.println(numero);
     */
-    }
+    
+    // Incremento e Decremento 
+    // int numero = 5;
+    // numero = numero + 1;
+    // numero ++;
+    // System.out.println(numero);
+    // System.out.println(numero--);
+    // System.out.println(numero);
+    
+    //boleano
+    // boolean variavel = true;
+    // System.out.println(!variavel);
+    // System.out.println(variavel);
+    // variavel = !variavel;
+    // System.out.println();
+    
+    //Operador ternario 
+    //  int a, b;
+    //  a = 5;
+    //  b = 6;
+    // String resultado = "";
+    // if(a==b)
+    //     resultado = "verdadeiro";
+    // else
+    //     resultado = "falso";
+    // System.out.println(resultado);
+    //SOLUÇÃO COM OPERADOR TERNARIO
+    // String resultado = a==b ? "Verdadeiro" : "Falso";
+    // System.out.println();
+    
+    //Operadores Relacionais 
+    // int numero1 = 1;
+    // int numero2 = 2;
+    // boolean simNao = numero1 == numero2;
+    // System.out.println("nummero1 é igual a numero2\n" + simNao);
+    // simNao = numero1 != numero2;
+    // System.out.println("nummero1 é igual a numero2\n" + simNao);
+    // simNao = numero1 > numero2;
+    // System.out.println("numero1 é mairo que numero2\n" + simNao);
+
+    //comparando objetos
+    //String nomeUm = "Natalia";
+    // String nomeDois = "Natalia";
+    // System.out.println(nomeUm == nomeDois); // retorna true
+    //String nomeDois = new String("Natalia");
+    //System.out.println(nomeUm == nomeDois); // retorna false
+    // maneira correta de comparar objetos
+    //System.out.println(nomeUm.equals(nomeDois)); // retorna true
+
+    //Operadores logicos
+    // boolean condicao1 = true;
+    // boolean condicao2 = false;
+    // if (condicao1 && condicao2){
+    //     System.out.println("As duas condições são verdadeira");
+    // }
+    // if (condicao1 || condicao2) {
+    //     System.out.println("Uma das condiçoões é verdadeira");
+    //}
+
 
     
+
+
+
+
+    }
 }
